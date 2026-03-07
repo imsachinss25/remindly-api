132e9fef47c180933b56e758ebbe3cf3129b9ad5
Sachin Soni <sachinsoni@Sachins-MacBook-Air.local>
2026-03-08 03:37:01 +0530

    added logs

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -4,6 +4,9 @@ const transporter = nodemailer.createTransport({
   host: process.env.BREVO_HOST,
   port: process.env.BREVO_PORT,
   secure: false,
+  connectionTimeout: 15000,
+  greetingTimeout: 15000,
+  socketTimeout: 15000,
   auth: {
     user: process.env.BREVO_LOGIN,
     pass: process.env.BREVO_SMTP_KEY,
@@ -11,12 +14,18 @@ const transporter = nodemailer.createTransport({
 });
 
 const sendEmail = async (to, subject, text) => {
+  console.log("mail logs >>>> ", {
+     host: process.env.BREVO_HOST,
+  port: process.env.BREVO_PORT,
+  from: process.env.BREVO_FROM,
+  })
   const resp = await transporter.sendMail({
     from: process.env.BREVO_FROM,
     to,
     subject,
     text,
   });
+  console.log("resp>>>> ", resp)
 };
 
 module.exports = sendEmail;
